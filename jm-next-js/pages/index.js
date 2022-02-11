a463d7086f9dccde8282781b94cbff1ed95041d4
jm <joemulick@gmail.com>
2022-02-11 02:04:30 -0800

    added app bar - removed gutters - xl container navbar limit

--- jm-next-js/pages/index.js
+++ jm-next-js/pages/index.js
@@ -1,24 +1,17 @@
 import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
+import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
+import Container from '@mui/material/Container';
+import AppBarResponsive from '../src/mui_components/AppBarResponsive'
 
 export default function Index() {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="false" disableGutters={true}>
+        <AppBarResponsive />
+        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+      </Container>
+    </React.Fragment>
   );
 }
